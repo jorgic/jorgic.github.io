bd7b62ed9d5f45bf5c43b62f9d633896b9fb0fc2
jorgadev <radovanjorgic14@gmail.com>
2021-04-05 10:53:59 +0200

    Github corner and react typing effect included

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import React from "react";
+import "./App.css";
+
+import ReactTypingEffect from "react-typing-effect";
+import GithubCorner from "react-github-corner";
+
+import gif from "./assets/main.gif";
+
 function App() {
-  return null;
+  return (
+    <>
+      <GithubCorner
+        href="https://github.com/jorgadev"
+        octoColor="#a891b7"
+        bannerColor="white"
+        target="_blank"
+      />
+      <div className="App">
+        <div className="wrapper">
+          <ReactTypingEffect
+            text={["Hello, I am jorgadev!", "Want to see my projects?"]}
+            className="title"
+            speed={100}
+            eraseSpeed={100}
+            eraseDelay={500}
+            typingDelay={1000}
+            displayTextRenderer={(text, i) => {
+              return (
+                <p>
+                  {text.split("").map((char, j) => {
+                    return (
+                      <span
+                        key={j}
+                        style={
+                          i === 0 && j > 11 && j < 20
+                            ? { fontWeight: "bold" }
+                            : {}
+                        }
+                      >
+                        {char}
+                      </span>
+                    );
+                  })}
+                </p>
+              );
+            }}
+          />
+          <img className="gif" src={gif} alt="Gif!" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
