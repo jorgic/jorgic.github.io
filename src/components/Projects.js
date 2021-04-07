5bb1f164d77308fdaedf98f15fb688a5624c3080
jorgadev <radovanjorgic14@gmail.com>
2021-04-07 12:17:44 +0200

    One project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,11 @@
 import "./Projects.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import HomeButton from "./HomeButton";
+import { FaPlay } from "react-icons/fa";
+import { FiCode } from "react-icons/fi";
+import musician from "../assets/musician.png";
 
 export default function Projects() {
   const settings = {
@@ -13,6 +15,7 @@ export default function Projects() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
+    draggable: true,
   };
 
   return (
@@ -20,23 +23,33 @@ export default function Projects() {
       <h1>Projects</h1>
       <div className="title-line"></div>
       <Slider {...settings} className="slider">
-        <div>
-          <h3>1</h3>
+        <div className="project mobile">
+          <div className="project-info">
+            <h1>MUSICIAN - GUESS THE SONG</h1>
+            <div className="title-line project-line"></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              rhoncus mi varius sapien laoreet consequat. Sed rhoncus in diam
+              vel interdum. Donec eleifend dapibus consequat. Cras cursus sit
+              amet purus sit amet placerat.
+            </p>
+            <div className="project-buttons">
+              <a className="link-btn">
+                <FaPlay />
+                Demo
+              </a>
+              <a className="link-btn">
+                <FiCode />
+                Code
+              </a>
+            </div>
+          </div>
+          <div className="project-picture">
+            <img className="device" src={musician} />
+          </div>
         </div>
-        <div>
+        <div className="project">
           <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
         </div>
       </Slider>
       <HomeButton />
