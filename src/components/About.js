6008f86a9c4ff6c6d2c64df8521a7b449de28cd1
jorgadev <radovanjorgic14@gmail.com>
2021-04-07 20:42:03 +0200

    About me finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import "./About.css";
 import HomeButton from "./HomeButton";
 import ParticlesBG from "./ParticlesBG";
+import Skills from "./Skills";
 
 export default function About() {
   return (
     <div className="About">
-      <h1>About Me</h1>
-      <div className="title-line"></div>
+      <div className="about-side ">
+        <h1>About Me</h1>
+        <div className="title-line"></div>
+        <p className="bio">
+          Yo!
+          <br />
+          <br /> My name is <strong>Radovan Jorgić</strong>. I am currently
+          living and studying in Ravne na Koroškem, Slovenia. <br />
+          <br />
+          From very young age I've shown passion about technology. I've been
+          programming for three years now and my love for it grows every day.
+          Aside of programming, I like football, futsal, music and a couple of
+          beers with friends.
+        </p>
+        <Skills />
+      </div>
+      <div className="right-side">
+        <p className="quote ">
+          “If you're the smartest person in the room, you're in the wrong room."
+        </p>
+      </div>
       <ParticlesBG />
       <HomeButton />
     </div>
---
+++ src/components/Skills.js
@@ -0,0 +1,34 @@
+import "./Skills.css";
+
+export default function Skills() {
+  return (
+    <div className="Skills">
+      <section>
+        <h3>Frontend</h3>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3 (SASS)</li>
+          <li>Javascript (jQuery)</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Backend</h3>
+        <ul>
+          <li>Node</li>
+          <li>PHP</li>
+          <li>Flask</li>
+          <li>Django</li>
+          <li>SQL</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Frameworks</h3>
+        <ul>
+          <li>React</li>
+          <li>React Native</li>
+          <li>Express</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
