321b3ea703f1bbab8fc16c016c9ea96a823d32d4
jorgadev <radovanjorgic14@gmail.com>
2021-04-09 19:58:47 +0200

    New CVs added

--- src/components/CVButton.js
+++ src/components/CVButton.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import ReactiveButton from "reactive-button";
 
-import cv from "../assets/cv.pdf";
+import cv_en from "../assets/EN_CV_Radovan_Jorgić.pdf";
+import cv_rs from "../assets/RS_CV_Radovan_Jorgić.pdf";
 import "animate.css";
 
 export default function CVButton({ language }) {
@@ -9,18 +10,22 @@ export default function CVButton({ language }) {
   let idleText = "";
   let loadingText = "";
   let successText = "";
+  let cv = null;
   if (language === "en") {
     idleText = "Download CV";
     loadingText = "Loading";
     successText = "Success";
+    cv = cv_en;
   } else if (language === "sl") {
     idleText = "Prenesi CV";
     loadingText = "Nalaganje..";
     successText = "Uspešno!";
+    cv = cv_rs;
   } else if (language === "rs") {
     idleText = "Preuzmi CV";
     loadingText = "Sačekajte..";
     successText = "Uspešno!";
+    cv = cv_rs;
   }
 
   const handleDownload = () => {
